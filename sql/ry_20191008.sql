6bdb72b4b0c8051511fc9dbbaa1fc0c629dc0fad
你çThisSeagull <957872488@qq.com>
2022-10-14 14:14:06 +0800

    sql replace

--- sql/ry_20191008.sql
+++ sql/ry_20191008.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 800311
 File Encoding         : 65001
 
-Date: 2022-10-14 14:02:12
+Date: 2022-10-14 14:02:12 11
 */
 
 SET FOREIGN_KEY_CHECKS=0;
